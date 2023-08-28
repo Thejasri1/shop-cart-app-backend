e4de44c5c0b097b010fd6ca201bbb9888359ed8a
thejasri1 <thejasrimadishetti@gmail.com>
2023-08-28 13:02:14 +0530

    local host changed

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ const app = express();
 //midlewares
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+
 //Mongoose connection
 const env = process.env.NODE_ENV || "development";
 const URL = env === "development" ? process.env.DB_URL : process.env.PROD_URL;
